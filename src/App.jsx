2f9128fbafdd36eb5be4c9e03a5e4864bce4d589
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-03-17 10:34:34 +0700

    buat beberapa perubhan pada tampilan

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,24 @@ import Workflow from "./components/Workflow";
 import Footer from "./components/Footer";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [Scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    if (!Scroll) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "auto";
+    }
+  }, [Scroll]);
+
   return (
     <>
       <Navbar />
-      <div className="max-w-7xl mx-auto pt-20 px-6">
-        <HeroSection />
+      <div className="max-w-7xl mx-auto pt-20 px-6 overflow-x-hidden">
+        <HeroSection setScroll={setScroll} />
         <FeatureSection />
         <Workflow />
         <Pricing />
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,41 +1,41 @@
-import video1 from "../assets/video1.mp4";
-import video2 from "../assets/video2.mp4";
 import { useRef } from "react";
-import video3 from "../assets/video3.mp4";
+import main from "../assets/background.mp4";
 import { motion, useInView } from "framer-motion";
+import arrow from "../assets/arrow.png";
 
-const HeroSection = () => {
-
+const HeroSection = ({ setScroll }) => {
   const ref1 = useRef(null);
   const ref2 = useRef(null);
   const isInView1 = useInView(ref1, { once: true });
   const isInView2 = useInView(ref2, { once: true });
 
   return (
-    <div className="flex flex-col items-center mt-6 lg:mt-20">
+    <div className="flex flex-col items-center lg:mt-20">
       <video
         autoPlay
         loop
         muted
-        className="absolute inset-0 w-full h-full object-cover blur-md -z-10"
+        className="absolute inset-0 w-full h-full object-cover blur-sm -z-10"
       >
-        <source src={video3} type="video/mp4" />
+        <source src={main} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl lg:-mt-10 text-center tracking-wide">
         We Never Stop Making
         <span className="bg-gradient-to-r from-red-700 to-indigo-400 text-transparent bg-clip-text px-4 py-1 border-2 border-transparent">
           Cool Things
         </span>
       </h1>
-      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
-        Shaping the future of technology and Web3 innovation,
-        we are on a mission to break boundaries and create limitless possibilities.
-        With a vision of global expansion, we harness the power of blockchain to build cutting-edge solutions,
-        secure funding, and unlock new revenue models. Our commitment to innovation goes beyond technology—we empower communities,
-        drive engagement through NFTs and the $HEXA token, and redefine the way the world interacts with Web3.
-        The future is here, and we’re building it together.
+      <p className="mt-10 text-lg text-center text-white max-w-4xl">
+        Shaping the future of technology and Web3 innovation, we are on a
+        mission to break boundaries and create limitless possibilities. With a
+        vision of global expansion, we harness the power of blockchain to build
+        cutting-edge solutions, secure funding, and unlock new revenue models.
+        Our commitment to innovation goes beyond technology—we empower
+        communities, drive engagement through NFTs and the $HEXA token, and
+        redefine the way the world interacts with Web3. The future is here, and
+        we’re building it together.
       </p>
       <div className="flex justify-center my-10">
         <a
@@ -48,35 +48,13 @@ const HeroSection = () => {
           Documentation
         </a>
       </div>
-      <div className="flex mt-10 justify-center" id="Doc">
-        <motion.video
-          ref={ref1}
-          initial={{ opacity: 0, x: -100 }}
-          animate={isInView1 ? { opacity: 1, x: 0 } : {}}
-          transition={{ duration: 1, ease: "easeOut" }}
-          autoPlay
-          loop
-          muted
-          className="rounded-lg w-1/2 border border-red-700 shadow-sm shadow-orange-400 mx-2 my-4"
-        >
-          <source src={video1} type="video/mp4" />
-          Your browser does not support the video tag.
-        </motion.video>
-
-        <motion.video
-          ref={ref2}
-          initial={{ opacity: 0, x: 100 }}
-          animate={isInView2 ? { opacity: 1, x: 0 } : {}}
-          transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
-          autoPlay
-          loop
-          muted
-          className="rounded-lg w-1/2 border border-red-700 shadow-sm shadow-orange-400 mx-2 my-4"
-        >
-          <source src={video2} type="video/mp4" />
-          Your browser does not support the video tag.
-        </motion.video>
-      </div>
+      <a
+        href="#Feature"
+        className="cursor-pointer hover:scale-110 flex flex-col items-center -mt-2"
+      >
+        <button onClick={() => setScroll(true)}>Scroll Down</button>
+        <img className="w-5 h-5 animate-bounce" src={arrow} />
+      </a>
     </div>
   );
 };
