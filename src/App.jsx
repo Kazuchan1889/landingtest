59e0efcdf5067af9a159b98613a8d3934119c9b1
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-04-10 23:26:01 +0700

    preload

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,35 @@ import Workflow from "./components/Workflow";
 import Footer from "./components/Footer";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
+import Preloader from "./components/Preloader";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <div className="max-w-7xl mx-auto pt-20 px-6">
-        <HeroSection />
-        <FeatureSection />
-        <Workflow />
-        <Pricing />
-        <Testimonials />
-        <Footer />
-      </div>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <>
+          <Navbar />
+          <div className="max-w-7xl mx-auto pt-20 px-6 overflow-x-hidden">
+            <HeroSection />
+            <FeatureSection />
+            <Workflow />
+            <Pricing />
+            <Testimonials />
+            <Footer />
+          </div>
+        </>
+      )}
     </>
   );
 };
